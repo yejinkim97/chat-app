35d6e2e2d4de7e646bd3143efc15a19fa049073d
Yejin <yejinkim5979@gmail.com>
2022-02-19 22:17:30 -0500

    init firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDFu_LcjTBM2WxitMjFFarYan-l9KSqf2w',
+  authDomain: 'chat-web-app-9b5ff.firebaseapp.com',
+  projectId: 'chat-web-app-9b5ff',
+  storageBucket: 'chat-web-app-9b5ff.appspot.com',
+  messagingSenderId: '797619901267',
+  appId: '1:797619901267:web:e60e3c9430576769cbc7a6',
+};
+
+const app = firebase.initializeApp(config);
